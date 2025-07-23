000ea86ef916f978c6862a39be930f3f119e5e0c
MacBook Air <macbookair@MacBooks-MacBook-Air-2.local>
2025-07-23 16:39:12 +0800

    fixed api

--- buy/src/app/(customer)/page.tsx
+++ buy/src/app/(customer)/page.tsx
@@ -2,24 +2,12 @@
 import AdminProfile from "./_components/AdminProfile";
 import Transactions from "./_components/Transactions";
 import SideBar from "./_components/SideBar";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.error("API Error:", err));
-  }, []);
   return (
     <div className="w-full flex">
       <SideBar />
-      <div>
-        <h1>Product List</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+
       <div className="flex flex-col gap-8 pt-11 w-full px-8 md:px-20">
         <AdminProfile />
         <Transactions />
